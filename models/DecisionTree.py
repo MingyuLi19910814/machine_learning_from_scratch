52f221b2988b145c0ff7c6aad59aaf65882e6524
MingyuLi <mingyuli19910814@gmail.com>
2022-03-28 21:34:40 +0900

    update linear regression

--- models/DecisionTree.py
+++ models/DecisionTree.py
@@ -1,78 +1,115 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
 from collections import Counter
 
-class Node:
-    def __init__(self, X, Y, currentHeight, maxHeight):
-        self.H = currentHeight
-        self.label = None
+
+class BaseNode:
+    def __init__(self, X, Y, height, max_height):
+        self.leaf_value = None
+        self.feature_idx = None
+        self.feature_dim = X.shape[1]
         self.threshold = None
-        self.featureIdx = None
-        self.__build__(X, Y, maxHeight)
-    @staticmethod
-    def __entropy__(Y):
-        p = np.asarray(list(Counter(Y).values()), dtype=float) / Y.shape[0]
-        return -np.sum(p * np.log(p))
+        self.left = None
+        self.right = None
+        self.__build__(X, Y, height, max_height)
 
-    @staticmethod
-    def __split__(X, Y, featureIdx, threshold):
-        Y1 = Y[X[:, featureIdx] < threshold]
-        Y2 = Y[X[:, featureIdx] >= threshold]
-        return Node.__entropy__(Y1) + Node.__entropy__(Y2)
+    def __createLeafNode__(self, X, Y):
+        raise NotImplementedError()
 
-    def __build__(self, X, Y, maxHeight):
-        if np.unique(Y).shape[0] == 1:
-            self.label = Y[0]
-            return
-        elif self.H == maxHeight:
-            self.label = Counter(Y).most_common(1)[0][0]
+    def __calc__(self, Y):
+        raise NotImplementedError()
+
+    def __create_child__(self, X, Y, height, max_height):
+        raise NotImplementedError()
+
+    def __build__(self, X, Y, height, max_height):
+        if height == max_height or np.unique(Y).size == 1:
+            self.__createLeafNode__(X, Y)
             return
 
-        bestEntropy = Node.__entropy__(Y)
-
-        for featureIdx in range(X.shape[1]):
-            for threshold in np.unique(X[:, featureIdx]):
-                entropy = Node.__split__(X, Y, featureIdx, threshold)
-                if entropy < bestEntropy:
-                    bestEntropy = entropy
-                    self.featureIdx = featureIdx
-                    self.threshold = threshold
-
-        if self.featureIdx is None:
-            self.label = Counter(Y).most_common(1)[0][0]
+        best_v = self.__calc__(Y)
+        for feature_idx in range(self.feature_dim):
+            feature_values = np.unique(X[:, feature_idx])
+            for feature_value in feature_values:
+                i1 = X[:, feature_idx] < feature_value
+                i2 = X[:, feature_idx] >= feature_value
+                v1 = self.__calc__(Y[i1])
+                v2 = self.__calc__(Y[i2])
+                v = (np.sum(i1) * v1 + np.sum(i2) * v2) / X.shape[0]
+                if v < best_v:
+                    best_v = v
+                    self.feature_idx = feature_idx
+                    self.threshold = feature_value
+        if self.feature_idx is None:
+            self.__createLeafNode__(X, Y)
             return
-        X1 = X[X[:, self.featureIdx] < self.threshold]
-        Y1 = Y[X[:, self.featureIdx] < self.threshold]
-        X2 = X[X[:, self.featureIdx] >= self.threshold]
-        Y2 = Y[X[:, self.featureIdx] >= self.threshold]
-        self.left = None if X1.shape[0] == 0 else Node(X1, Y1, self.H + 1, maxHeight)
-        self.right = None if X2.shape[0] == 0 else Node(X2, Y2, self.H + 1, maxHeight)
+        i1 = X[:, self.feature_idx] < self.threshold
+        i2 = X[:, self.feature_idx] >= self.threshold
+        self.left = self.__create_child__(X[i1], Y[i1], height + 1, max_height)
+        self.right = self.__create_child__(X[i2], Y[i2], height + 1, max_height)
+
+class EntropyNode(BaseNode):
+    def __createLeafNode__(self, X, Y):
+        self.leaf_value = Counter(Y).most_common(1)[0][0]
+
+    def __calc__(self, Y):
+        p = np.array(list(Counter(Y).values())).astype(float) / Y.shape[0]
+        return np.sum(-p * np.log(p + 1e-13))
+
+    def __create_child__(self, X, Y, height, max_height):
+        return EntropyNode(X, Y, height, max_height)
+
+class GiniNode(BaseNode):
+    def __createLeafNode__(self, X, Y):
+        self.leaf_value = Counter(Y).most_common(1)[0][0]
+
+    def __calc__(self, Y):
+        p = np.array(list(Counter(Y).values())).astype(float) / Y.shape[0]
+        return 1 - np.sum(p * p)
+
+    def __create_child__(self, X, Y, height, max_height):
+        return GiniNode(X, Y, height, max_height)
 
 class DecisionTree:
-    def __init__(self, X, Y, maxHeight=-1):
-        X = np.asarray(X)
-        Y = np.asarray(Y)
-        if X.ndim == 1:
-            X = X.reshape((-1, 1))
-        if Y.ndim != 1 or X.shape[0] != Y.shape[0]:
-            raise ValueError("input error")
-        self.featureDim = X.shape[1]
-        self.node = Node(X, Y, 0, maxHeight)
-    def predict(self, X):
-        X = np.asarray(X)
-        if X.ndim == 1:
-            X = X.reshape((1, -1))
-        if X.shape[1] != self.featureDim:
-            raise ValueError("input error")
+    def __init__(self, X, Y, max_height=-1):
+        X = np.asarray(X).astype(float)
+        Y = np.asarray(Y).astype(int)
+        if X.shape[0] != Y.shape[0]:
+            raise ValueError("different length of X and Y!")
+        elif X.shape[0] == 0:
+            raise ValueError("train X must not be empty")
+        elif X.ndim != 2:
+            raise ValueError("X must be 2-dimensional")
+        elif Y.ndim != 1:
+            raise ValueError("Y must be 1-dimensional")
+        self.node = GiniNode(X, Y, 0, max_height)
 
+    def predict(self, X):
+        X = np.asarray(X).astype(float)
+        X = X.reshape((-1, self.node.feature_dim))
         ans = []
         for x in X:
             node = self.node
-            while node.label is None:
-                node = node.left if x[node.featureIdx] < node.threshold else node.right
-            ans.append(node.label)
-        return np.asarray(ans)
+            while node.leaf_value is None:
+                feature_idx = node.feature_idx
+                threshold = node.threshold
+                node = node.left if x[feature_idx] < threshold else node.right
+            ans.append(node.leaf_value)
+        return np.array(ans)
+
+
 if __name__ == "__main__":
-    X = np.arange(0, 100)
-    Y = np.concatenate([np.zeros(50), np.ones(50)], axis=0)
-    tree = DecisionTree(X, Y, 3)
-    print(tree.predict(np.arange(25, 75).reshape((-1, 1))))
+    dataset = load_iris()
+    X = dataset.data
+    Y = dataset.target
+    acc = []
+    for random_state in range(43):
+        train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size=0.25, random_state=random_state)
+        tree = DecisionTree(train_x, train_y)
+        pred_y = tree.predict(test_x)
+        accuracy = accuracy_score(test_y, pred_y)
+        acc.append(accuracy)
+        print('state = {}, acc = {}'.format(random_state, accuracy))
+    print('average acc = {}'.format(np.mean(acc)))
--- models/LinearRegression.py
+++ models/LinearRegression.py
@@ -1,6 +1,11 @@
 import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from statsmodels.regression.linear_model import OLS
 
-class LinearRegression:
+class LinearRegressionLeastSquare:
     def __init__(self, X, Y):
         self.X = np.asarray(X)
         self.Y = np.asarray(Y)
@@ -23,12 +28,94 @@ class LinearRegression:
         meanY = np.mean(self.Y)
         self.slope = np.sum((self.X - meanX) * (self.Y - meanY)) / np.sum((self.X - meanX) ** 2)
         self.intersection = meanY - self.slope * meanX
+        #print(self.slope, self.intersection)
+
+class LinearRegressionGradientDescent:
+    def __init__(self):
+        self.w = None
+        self.b = 0
+        self.r2 = None
+
+    def __predict__(self, X):
+        return np.dot(X, self.w) + self.b
+
+    def __error__(self, Y, y_pred):
+        return 0.5 * np.mean(np.square((Y - y_pred)))
+
+    def fit(self, X, Y, lr, max_it):
+        X = np.asarray(X).astype(float)
+        Y = np.asarray(Y).astype(float)
+
+        if X.ndim == 1:
+            X = X.reshape((Y.shape[0], -1))
+        self.w = np.random.randn(X.shape[1])
+        train_err = []
+        for it in range(max_it):
+            pred_y = self.__predict__(X)
+            err = self.__error__(Y, pred_y)
+            train_err.append(err)
+
+            dw = np.dot(X.T, pred_y - Y) / X.shape[0]
+            db = np.mean(pred_y - Y)
+            self.w -= lr * dw
+            self.b -= lr * db
+        pred_y = self.__predict__(X)
+        mean_y = np.mean(Y)
+        self.r2 = 1 - np.sum(np.square(pred_y - Y)) / np.sum(np.square(mean_y - Y))
+        print('W = {}, b = {}'.format(self.w, self.b))
+        assert np.abs(self.r2 - r2_score(Y, pred_y)) <= 1e-13
+
+def plot(X, Y, w, b):
+    plt.scatter(X, Y)
+    y_pred = np.dot(X, w) + b
+    plt.plot(X, y_pred)
+    plt.show()
+
+def collinearityCheck(X):
+    print('*' * 40)
+    print('collinearity check!')
+    def calc_vif(X, idx):
+        y = X[:, idx]
+        include_idx = np.arange(X.shape[1]) != idx
+        x = X[:, include_idx]
+        lr = LinearRegression()
+        lr.fit(x, y)
+        y_pred = lr.predict(x)
+        r2 = r2_score(y, y_pred)
+        vif = 1 / (1 - r2)
+        return vif
+    for feature_idx in range(X.shape[1]):
+        vif = calc_vif(X, feature_idx)
+        print('idx = {}, vif = {}'.format(feature_idx, vif))
+
+
+def homoscedasticityCheck(Y, y_pred):
+    print('*' * 40)
+    print('homoscedasticity check!')
+    res = Y - y_pred
+    plt.scatter(Y, res)
+    plt.show()
+
+def normalDistributionCheck(Y, y_pred):
+    print('*' * 40)
+    print('normal distribution check!')
+    res = Y - y_pred
+    plt.hist(res)
+    plt.show()
 
 if __name__ == "__main__":
-    slope = 3
+    W = np.array([-3, 5, 1, 2, 4])
     intersect = -30
-    X = np.arange(0, 100)
-    Y = slope * X + intersect + np.random.rand(*X.shape)
-    regression = LinearRegression(X, Y)
-    print(regression.getSlope())
-    print(regression.getIntersect())
+    X = np.random.randn(100, W.shape[0])
+    Y = np.dot(X, W) + intersect + np.random.randn(X.shape[0]) * 0.05
+    collinearityCheck(X)
+    regression = LinearRegressionGradientDescent()
+    regression.fit(X, Y, 0.01, 5000)
+    y_pred = regression.__predict__(X)
+
+    homoscedasticityCheck(Y, y_pred)
+    normalDistributionCheck(Y, y_pred)
+
+    # regSklearn = LinearRegression()
+    # regSklearn.fit(X, Y)
+    # print(regSklearn.coef_, regSklearn.intercept_)
